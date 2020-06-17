1a5b910ee132160ab7627e9623c76929088ec1ff
paul4912 <paulyang4912@gmail.com,au>
2020-06-17 19:21:24 +1000

    Feat: Add Delete endpoint

---
+++ src/DotNetServerless.Application/Requests/DeleteNoteRequest.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using DotNetServerless.Application.Responses;
+using MediatR;
+using System;
+
+namespace DotNetServerless.Application.Requests
+{
+    public class DeleteNoteRequest : IRequest<DeleteNoteResponse>
+    {
+        [DynamoDBHashKey]
+        public string userId { get; set; }
+        [DynamoDBRangeKey]
+        public Guid noteId { get; set; }
+    }
+}
---
+++ src/DotNetServerless.Lambda/Functions/DeleteNoteFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using DotNetServerless.Application.Requests;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace DotNetServerless.Lambda.Functions
+{
+    public class DeleteNoteFunction
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DeleteNoteFunction() : this(Startup
+          .BuildContainer()
+        .BuildServiceProvider())
+        {
+        }
+
+        public DeleteNoteFunction(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<APIGatewayProxyResponse> Run(APIGatewayProxyRequest request)
+        {
+            var requestModel = new DeleteNoteRequest { noteId = new Guid(request.PathParameters["noteId"]), userId = request.Headers["userId"] };
+
+            var mediator = _serviceProvider.GetService<IMediator>();
+
+            var result = await mediator.Send(requestModel);
+
+            return result == null ?
+              new APIGatewayProxyResponse { StatusCode = 404 } :
+              new APIGatewayProxyResponse { StatusCode = 200, Body = JsonConvert.SerializeObject(result) };
+        }
+    }
+}
